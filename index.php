424f110fd3641e19092d5ad781ef5e59a5f78bdf
Naseem <naseemshahvarikodan@gmail.com>
2020-03-28 10:51:06 +0530

    Login added

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doctor Patient Record System</title>
+    <!--Bootstrap Dependencies-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <!--   -->
+</head>
+<body class="bg-dark">
+    <section class="mainsection jumbotron bg-dark">
+    <h1 class="text-white">Doctor Patient Record System</h1>
+    <p class="text-light">This project is an academic assignment for DBMS course
+    in Cochin University of Science and Technology. We here propose a
+    doctor patient handling, managing system that helps doctors
+    in their work and also patients to view medical progress. Each
+    time a user visits a doctor his/her medical entry is stored in the database by doctor. 
+    Next time a user logs in he may view his/her entire medical history as and when needed. 
+    At the same time a doctor may view patients medical history even bore the patient visits him. 
+    This allows for an automated patient doctor handling system through an online interface.</p>
+    
+    <h3 class="text-white">Contributors:</h3>
+    <div class="row">
+        <div class="col col-lg-1 col-md-1 col-sm-3 col-3 ">
+            <img src="assets/img/naseem.jpg" alt="" class="dp rounded-circle img-thumbnail">
+            <p class="name font-weight-bold text-light my-2 mx-auto ">Naseem Shah - @ns4k_tv</p>
+        </div>
+        <div class="col col-lg-1 col-md-1 col-sm-3 col-3 ">
+            <img src="assets/img/naseem.jpg" alt="" class="dp rounded-circle img-thumbnail">
+            <p class="name font-weight-bold text-light my-2 mx-auto ">Naseem Shah - @ns4k_tv</p>
+        </div>
+        <div class="col col-lg-1 col-md-1 col-sm-3 col-3 ">
+            <img src="assets/img/naseem.jpg" alt="" class="dp rounded-circle img-thumbnail">
+            <p class="name font-weight-bold text-light my-2 mx-auto ">Naseem Shah - @ns4k_tv</p>
+        </div>
+        <div class="col col-lg-1 col-md-1 col-sm-3 col-3 ">
+            <img src="assets/img/naseem.jpg" alt="" class="dp rounded-circle img-thumbnail">
+            <p class="name font-weight-bold text-light my-2 mx-auto ">Naseem Shah - @ns4k_tv</p>
+        </div>
+        
+    </div>
+    <h3 class="text-white">Stack:</h3>
+    <div class="row">
+        <div class="col  col-4">
+            
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title">Front-end</h5>
+                <ul>
+                        <li>HTML+CSS</li>
+                        <li>Bootstrap</li>
+                </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col  col-4">
+            
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title">Database</h5>
+                <ul>
+                        <li>SQL</li>
+                        
+                </ul>
+                    <br>
+                </div>
+            </div>
+        </div>
+        <div class="col  col-4">
+            
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title">Backend</h5>
+                <ul>
+                    <li>PHP</li>
+                </ul>
+                <br>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    <br><br>
+    <div class="row">
+        <div class="col align-self-center">
+
+            <a href="/login" class="btn btn-primary">Start Project</a>
+        </div>
+    </div>
+    
+
+    </section>
+</body>
+</html>
